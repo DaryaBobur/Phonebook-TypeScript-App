484d14a06c2ddd61bea017a5392d6c7c020a497f
Daria <97470059+DaryaBobur@users.noreply.github.com>
2023-07-29 11:13:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-    <BrowserRouter basename="Phonebook-React-App">
+    <BrowserRouter basename="Phonebook-TypeScript-App">
       <App />
       </BrowserRouter>
       </PersistGate>
